f25b43f97af5399a824879b1cedb3fd6e572803c
SolveySantacruz <ssantacruz@unal.edu.co>
2022-08-24 15:54:38 -0500

    Cambios

--- CLASES/Practica1.py
+++ CLASES/Practica1.py
@@ -11,3 +11,7 @@ print('Variable=',12,'Este es un número importante',sep='\n') #Salto de línea
 print('Variable=',12,'Este es un número importante',sep='###')
 
 #git--version
+# Configurar el usuario de GitHub: escribir en el terminal
+# git config --global user.name 'SolveySantacruz'
+# git config --global user.email 'ssantacruz@unal.edu.co'
+
