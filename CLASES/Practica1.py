0704f916e181a23db3f6a0bc0195d54123d81dc2
SolveySantacruz <ssantacruz@unal.edu.co>
2022-08-24 15:33:14 -0500

    Este es el repositorio de clase

---
+++ CLASES/Practica1.py
@@ -0,0 +1,13 @@
+# Estamos utilizando la función print para ejecutar la línea de código
+# más familiar en todos los lenguajes
+
+print('Hola mundo')
+print(True)
+print(12.54)
+print([1,2,3])
+print((1,2,3))
+
+print('Variable=',12,'Este es un número importante',sep='\n') #Salto de línea
+print('Variable=',12,'Este es un número importante',sep='###')
+
+#git--version
